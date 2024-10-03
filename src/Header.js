5149dd058fc628327d7e6123dd19b0749ccb1318
thibault <breakthegreat@gmail.com>
2024-10-03 13:21:57 -0400

    added header file

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>{this.props.headerInfo.name}</div>;
+    return (
+      <div>
+        <div class="cv-header">
+          <div>
+            <h1>{this.props.headerInfo.name}</h1>
+            <h2> {this.props.headerInfo.field}</h2>
+          </div>
+          <div class="user-info">
+            <a href="mailto:tac47@njit.edu">{this.props.headerInfo.email}</a>
+            <p>{this.props.headerInfo.web}</p>
+            <p>{this.props.headerInfo.mobile}</p>
+          </div>
+        </div>
+        <hr class="cv-header-divider" />
+      </div>
+    );
   }
 }
 
