312fd8890286d836075f1a02ce4d2848670c318d
thibault <breakthegreat@gmail.com>
2024-10-03 13:36:18 -0400

    added skill

--- src/Skills.js
+++ src/Skills.js
@@ -3,58 +3,61 @@ import React, { Component } from "react";
 class Skills extends Component {
   render() {
     return (
-      <div class="section">
-        <h3>Key Skills</h3>
+      <div>
+        <div class="section">
+          <h3>Key Skills</h3>
 
-        <div>
-          <p>
-            {this.props.skill.skill}
+          <div>
+            <p>
+              {this.props.skill.skill}
 
-            <br />
-            {this.props.skill.skill}
+              <br />
+              {this.props.skill.skill}
 
-            <br />
-            {this.props.skill.skill}
+              <br />
+              {this.props.skill.skill}
 
-            <br />
-            {this.props.skill.skill}
+              <br />
+              {this.props.skill.skill}
 
-            <br />
-          </p>
+              <br />
+            </p>
+          </div>
+          <div>
+            <p>
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+            </p>
+          </div>
+
+          <div>
+            <p>
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+              {this.props.skill.skill}
+
+              <br />
+            </p>
+          </div>
         </div>
-        <div>
-          <p>
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-          </p>
-        </div>
-
-        <div>
-          <p>
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-            {this.props.skill.skill}
-
-            <br />
-          </p>
-        </div>
+        <hr />
       </div>
     );
   }
